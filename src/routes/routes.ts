10133e4966990f0d09de3136214fbbfb9b0fb55c
nicodeheza <nicolasdeheza@hotmail.com>
2022-07-28 17:52:25 -0300

    routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,13 @@
 import {Router} from "express";
+import addPhoto from "../controllers/addPhoto";
+import deletePhoto from "../controllers/deletePhoto";
+import isAdmin from "../middlewares/isAdmin";
+import getPhotos from "../controllers/getPhotos";
 
 const router = Router();
+
+router.post("/image", addPhoto);
+router.delete("/image", isAdmin, deletePhoto);
+router.get("/images", getPhotos);
 
 export default router;
